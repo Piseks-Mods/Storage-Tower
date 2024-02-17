c2919c65d0f9e71cb1304a833e7155bc5b6bf290
Písek Pískovec <pisekpiskovec@gmail.com>
2024-02-17 21:16:28 +0100

    Storage Elevator

---
+++ src/main/java/cz/pisekpiskovec/storagetower/procedures/StorageElevatorRightClickedProcedure.java
@@ -0,0 +1,116 @@
+package cz.pisekpiskovec.storagetower.procedures;
+
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+
+import java.util.Map;
+
+import io.netty.buffer.Unpooled;
+
+import cz.pisekpiskovec.storagetower.gui.StorageElevatorGUIGui;
+import cz.pisekpiskovec.storagetower.block.StorageCrateBlock;
+import cz.pisekpiskovec.storagetower.PiseksStorageTowerMod;
+
+public class StorageElevatorRightClickedProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				PiseksStorageTowerMod.LOGGER.warn("Failed to load dependency world for procedure StorageElevatorRightClicked!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				PiseksStorageTowerMod.LOGGER.warn("Failed to load dependency x for procedure StorageElevatorRightClicked!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				PiseksStorageTowerMod.LOGGER.warn("Failed to load dependency y for procedure StorageElevatorRightClicked!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				PiseksStorageTowerMod.LOGGER.warn("Failed to load dependency z for procedure StorageElevatorRightClicked!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				PiseksStorageTowerMod.LOGGER.warn("Failed to load dependency entity for procedure StorageElevatorRightClicked!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		boolean found = false;
+		double yLvl = 0;
+		found = (false);
+		if (new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos(x, y, z), "LevelInspector") == 0) {
+			yLvl = 0;
+			for (int index0 = 0; index0 < (int) (256); index0++) {
+				if ((world.getBlockState(new BlockPos(x, yLvl, z))).getBlock() == StorageCrateBlock.block) {
+					if (!world.isRemote()) {
+						BlockPos _bp = new BlockPos(x, y, z);
+						TileEntity _tileEntity = world.getTileEntity(_bp);
+						BlockState _bs = world.getBlockState(_bp);
+						if (_tileEntity != null)
+							_tileEntity.getTileData().putDouble("LevelInspector", yLvl);
+						if (world instanceof World)
+							((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+					}
+					found = (true);
+					break;
+				}
+				yLvl = (yLvl + 1);
+			}
+		} else {
+			found = (true);
+		}
+		if (found) {
+			{
+				Entity _ent = entity;
+				if (_ent instanceof ServerPlayerEntity) {
+					BlockPos _bpos = new BlockPos(x, y, z);
+					NetworkHooks.openGui((ServerPlayerEntity) _ent, new INamedContainerProvider() {
+						@Override
+						public ITextComponent getDisplayName() {
+							return new StringTextComponent("StorageElevatorGUI");
+						}
+
+						@Override
+						public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+							return new StorageElevatorGUIGui.GuiContainerMod(id, inventory, new PacketBuffer(Unpooled.buffer()).writeBlockPos(_bpos));
+						}
+					}, _bpos);
+				}
+			}
+		} else {
+			if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("No Storage Crate found in this tower"), (true));
+			}
+		}
+	}
+}
